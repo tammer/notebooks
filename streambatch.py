36276edde2fe3287d48571309cd79034d8e8d9a5
Tammer <tammer@tammer.com>
2023-08-04 11:52:58 -0400

    .

--- streambatch.py
+++ streambatch.py
@@ -6,48 +6,49 @@ import pyarrow.parquet # for reading parquet files (implicit dependency)
 import fsspec # for reading parquet files (implicit dependency)
 import s3fs # for reading parquet files (implicit dependency)
 
-DEBUG_NO_CALL = False # set to True to skip the API call and use exisint data on server
-
 class StreambatchData:
-    def __init__(self, api_key, space, time=None,asyncronous=False):
-
-        # if time is not given, set it to be the full history
-        if time is None:
-            time = time = {'start':'2015-01-01','end':'2053-08-01','unit':'day'} # !!!
-        self.time = time
-
+    def __init__(self, api_key, space, time=None,asyncronous=False,silent=False,debug=False):
+        
         # if space is not in a list, put it in a list
         if type(space[0]) is not list:
             space = [space]
         self.space = space
-        
+
+        if not silent: print("Number of locations: {}".format(len(space)))
+
+        # if time is not given, set it to be the full history
+        if time is None:
+            time = time = { 'start':'2014-01-01',
+                            'end':self.today(),
+                            'unit':'day'}
+        self.time = time
+
+        if not silent: print("Range: {} - {}, unit: {}".format(time['start'],time['end'],time['unit']))
+            
         # set the class variables
         self.api_header = {'X-API-Key': api_key}
         self.ndvi_data = None     # the data that is returned from the server
         self.result = None        # the result of the request. Will be None or the error message if we failed
         self.final_status = None  # starts as None and will become either 'Succeeded' or 'Failed'
 
-        if DEBUG_NO_CALL:
+        if debug:
             print("debug no call to server")
             self.query_id = '7877840e-6a54-4ab6-b5a9-a589dee03593'
             self.access_url = f's3://streambatch-data/{self.query_id}.parquet'
         else:
             ndvi_request = {'variable': ['ndvi.sentinel2'], 'space': space, 'time': time }
-            print(f"Request: {ndvi_request}")
             response = requests.post('https://api.streambatch.io/async', json=ndvi_request, headers=self.api_header)
             if response.status_code != 200:
                 print("Error: request failed")
                 print(f"Response: {response.status_code} {response.reason}")
                 print(response.content)
                 return
-            print("Processing request...")
-            # print(f"Response: {response.status_code} {response.reason}")
-            # print(response.content)
             self.query_id = json.loads(response.content)['id']
             self.access_url = json.loads(response.content)['access_url']
+            if not silent: print("Query ID: {}".format(self.query_id))
         
         if asyncronous is False:
-            print("Waiting for results...",end="")
+            print("Waiting for results...",end="",flush=True)
             self.wait_for_results()
 
     def as_dataframe(self):
@@ -78,11 +79,17 @@ class StreambatchData:
         while self.ready() is False:
             self.status() # causes self.ready to be updated
             time.sleep(7)
-            print('.',end="")
+            print('.',end="",flush=True)
+        print("")
+        print("Ready.")
+    
+    def today(self): return time.strftime("%Y-%m-%d")
+
+    
 
 api_key = open('key.txt').read().strip()
 space = [3.940705,49.345238]
-ndvi_data = StreambatchData(api_key, space)
+ndvi_data = StreambatchData(api_key, space,debug=False)
 print(ndvi_data.as_dataframe())
 
 # asynchonous request
